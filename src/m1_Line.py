e38d9a25841df66c24376ddb8578892eceb39111
lauterja <lauterja@rose-hulma.edu>
2019-01-15 09:57:58 -0500

    Done

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -448,8 +448,7 @@ class Line(object):
         #        The tests are already written (below).
         #        They include the Example in the above doc-string.
         # ---------------------------------------------------------------------
-        distance = ((self.end.y - self.start.y) ** 2 + self.end.x - self.start.x) ** 2) ** (1/2)
-        return distance
+        
     def get_number_of_clones(self):
         """
         What comes in:
